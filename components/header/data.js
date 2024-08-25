bef64e5e1db9fb76cf758fda4573cd7c811d9da0
Mariel Torres <marieltorres97@gmail.com>
2024-08-25 15:51:32 -0300

    first commit

---
+++ components/header/data.js
@@ -0,0 +1,45 @@
+export const navItems = [
+    'Acerca de',
+    'Proyectos',
+    'Repositorio',
+    'Novedades',
+    'Contacto',
+];
+export const lang = 'EN/ES'
+export const searchData = {
+    search: {
+        pathW: "/searchWhite.png",
+        pathB: "/buscador.png",
+        alt: "buscador"
+    },
+}
+export const imagesNav = {
+    logo: {
+        pathW: "/logo.png",
+        pathB: "/logoWhite.png",
+        pathDesktop: "/logoDesktop.png",
+        alt: "logo boron"
+    },
+
+    openMenu: {
+        pathOpen: "/menu.png",
+        pathClose: "/close.png",
+        alt: "menu"
+    },
+
+}
+
+export const network = {
+    arrow: {
+        path: '/arrow.png',
+        alt: "flecha"
+    },
+    x: {
+        path: "/x.png",
+        alt: "x"
+    },
+    youtube: {
+        path: "/youtube.png",
+        alt: "youtube"
+    },
+}
---
+++ components/header/domElements.js
@@ -0,0 +1,27 @@
+export const nav = document.getElementById("nav");
+export const ulNoScroll = document.createElement("div");
+export const logo = document.createElement("img");
+export const containerMenu = document.createElement("div");
+export const containerButtonMenu = document.createElement("div")
+export const menu = document.createElement("p");
+export const openMenu = document.createElement("img");
+
+export const containerLangSearch = document.createElement("div")
+export const language = document.createElement("p");
+export const search = document.createElement('img');
+
+export const containerLangSearchNoScroll = document.createElement("div")
+export const languageNoScroll = document.createElement("p");
+export const searchNoScroll = document.createElement('img');
+
+export const containerBtnNewsletter = document.createElement("div");
+export const buttonNewsLetter = document.createElement("button");
+export const newsletter = document.createElement("p")
+export const arrow = document.createElement('img');
+
+export const containerNetwork = document.createElement("div");
+export const email = document.createElement("p");
+
+export const containerSocialMedia = document.createElement("div");
+export const x = document.createElement("img");
+export const youtube = document.createElement("img");
